6cc4045334b805bae480670d095d3ca93ba959ca
freddiesdba <fredsako8@gmail.com>
2017-12-17 19:47:42 +0300

    routing

--- click/config/routes.rb
+++ click/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+# Default root url
+  root 'Pages#index' 
+# default for pages
+  get 'Pages/home'
+  get 'Pages/profile'
+  get 'Pages/explore'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
